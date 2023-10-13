87abe5e5e1b80675eadc8d435c355e1aa38f3e6c
Luis Yamada <luishm.yamada@gmail.com>
2023-10-13 11:08:05 -0300

    hpa implemented for marquez web ui

--- experiments/requests_overload/experiment.py
+++ experiments/requests_overload/experiment.py
@@ -3,11 +3,13 @@ import concurrent.futures
 
 
 marquez_url = "http://localhost:5000/api/v1"
+marquez_webui_url = "http://localhost:3000"
 infinite_counter = [i for i in range(10000000)]
 
 
 def hit_marquez():
-    response = requests.get(url=f'{marquez_url}/namespaces')
+    # response = requests.get(url=f'{marquez_url}/namespaces')
+    response = requests.get(url=f'{marquez_webui_url}')
     return response.status_code
 
 
@@ -16,7 +18,7 @@ def print_hit(infinite_counter):
 
 
 def send_concurrent_requests(infinite_counter):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         executor.map(print_hit, infinite_counter)
     
 
--- experiments/requests_overload/watch_hpa.sh
+++ experiments/requests_overload/watch_hpa.sh
@@ -1,1 +1,2 @@
 kubectl get hpa marquez --namespace=marquez --watch
+kubectl get hpa marquez-web --namespace=marquez --watch
